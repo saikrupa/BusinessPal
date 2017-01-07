0912a599822464f20c30d7f0a05aaf4b18cba38f
saikrupa <puram.saikrupa@gmail.com>
2017-01-07 19:36:26 +0530

    contacts commit

--- app/src/main/java/businesspal/saikrupa/com/businesspal/AddNewPayable.java
+++ app/src/main/java/businesspal/saikrupa/com/businesspal/AddNewPayable.java
@@ -2,16 +2,23 @@ package businesspal.saikrupa.com.businesspal;
 
 
 import android.app.DatePickerDialog;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import businesspal.saikrupa.com.businesspal.database.DatabaseHandler;
 import businesspal.saikrupa.com.businesspal.helper.AddPayableHelper;
@@ -26,7 +33,7 @@ public class AddNewPayable extends AppCompatActivity {
     EditText etFName;
     EditText etLName;
     EditText etAddress;
-    EditText etPhoneNo;
+    AutoCompleteTextView actvPhoneNo;
     EditText etAmount;
     TextView tvDate;
     String creditType;
@@ -51,10 +58,12 @@ public class AddNewPayable extends AppCompatActivity {
         etFName = (EditText) findViewById(R.id.etFName);
         etLName = (EditText) findViewById(R.id.etLName);
         etAddress = (EditText) findViewById(R.id.etAddress);
-        etPhoneNo = (EditText) findViewById(R.id.etNumber);
+        actvPhoneNo = (AutoCompleteTextView) findViewById(R.id.actvNumber);
         etAmount = (EditText) findViewById(R.id.etAmount);
         tvDate = (TextView) findViewById(R.id.tvDate);
         db = new DatabaseHandler(AddNewPayable.this);
+
+        actvPhoneNo.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.single_contact, R.id.tv_ContactNumber, getAllContactNames()));
 
     }
 
@@ -63,7 +72,7 @@ public class AddNewPayable extends AppCompatActivity {
         String lname = etLName.getText().toString();
         String address = etAddress.getText().toString();
         String amount = etAmount.getText().toString();
-        String phoneNo = etPhoneNo.getText().toString();
+        String phoneNo = actvPhoneNo.getText().toString();
         String date = tvDate.getText().toString();
 
         if (fname.length() > 0 && lname.length() > 0 && address.length() > 0 && amount.length() > 0 && phoneNo.length() > 0 && !date.equals("Date")) {
@@ -111,4 +120,22 @@ public class AddNewPayable extends AppCompatActivity {
         dialog.show();
 
     }
+
+    private List<String> getAllContactNames() {
+        List<String> lContactNamesList = new ArrayList<>();
+        try {
+            // Get all Contacts
+            Cursor lPeople = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            if (lPeople != null) {
+                while (lPeople.moveToNext()) {
+                    // Add Contact's Name into the List
+                    lContactNamesList.add(lPeople.getString(lPeople.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+                }
+            }
+        } catch (NullPointerException e) {
+            Log.e("getAllContactNames()", e.getMessage());
+        }
+        return lContactNamesList;
+    }
+
 }
